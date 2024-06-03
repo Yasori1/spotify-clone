2d0d6d67e5e5faf4fd73ab196b0b3275bf71d342
Eren <yasar451278@gmail.com>
2024-06-03 10:06:44 +0300

    Upload

---
+++ components/ListItem.tsx
@@ -0,0 +1,82 @@
+'use client';
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { FaPlay } from "react-icons/fa";
+
+interface ListItemProps {
+    image: string;
+    name: string;
+    href: string;
+}
+
+const ListItem: React.FC<ListItemProps> = ({
+    image,
+    name,
+    href
+}) => {
+    const router = useRouter();
+
+    const onClick = () => {
+        // Add authentication before push
+        router.push(href);
+    }
+
+    return (
+        <button
+        onClick={onClick}
+            className="
+            relative
+            group
+            flex
+            items-center
+            rounded-md
+            overflow-hidden
+            gap-x-4
+            bg-neutral-100/10
+            hover:bg-neutral-100/20
+            transition
+            pr-4
+            "            
+        >
+            <div className="
+                relative
+                min-h-[64px]
+                min-w-[64px]                   
+            ">
+              <Image
+    className="object-cover"
+    src={image}
+    alt="Image"
+    width={64} 
+    height={64} 
+    bg-grey
+/>
+
+            </div>
+            <p className="font-medium truncate py-5">
+                {name}
+            </p>
+            <div 
+            className="
+            absolute
+            transition
+            opacity-0
+            rounded-full
+            flex
+            items-center
+            bg-emerald-700
+            p-4
+            drop-shadow-md
+            right-5
+            group-hover:opacity-100
+            hover:scale-100
+            "
+            >
+                <FaPlay className="text-orange-700"/>
+            </div>           
+        </button>
+    );
+}
+
+export default ListItem;
