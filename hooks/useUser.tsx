372e9df2a4fb45ca8beee8bbfe95314079ca6a04
Eren <yasar451278@gmail.com>
2024-06-05 17:44:09 +0300

    updated

--- hooks/useUser.tsx
+++ hooks/useUser.tsx
@@ -3,10 +3,11 @@ import {
   useSessionContext,
   useUser as useSupaUser,
 } from "@supabase/auth-helpers-react";
-import { createContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 import { Subscription, UserDetails } from "@/types";
 import { Result } from "postcss";
+import { error } from "console";
 
 type UserContextType = {
   accessToken: string | null;
@@ -74,10 +75,17 @@ export const MyUserContextProvider = (props: Props) => {
     user,
     userDetails,
     isLoading: isLoadingUser || isLoadingData,
-    subscription
+    subscription,
   };
 
-  return <UserContext.Provider value ={value} {...props} />
+  return <UserContext.Provider value={value} {...props} />;
 };
 
+export const useUser = () => {
+  const context = useContext(UserContext);
+  if (context === undefined) {
+    throw new Error("useUser must be used within a MyUserContextProvider");
+  }
 
+  return context;
+};
