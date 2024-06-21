e6652456107a1b678bf27fe79f83d759f78501c7
Eren <yasar451278@gmail.com>
2024-06-22 01:09:27 +0300

    up

--- components/Header.1.tsx
+++ components/Header.1.tsx
@@ -1,14 +1,16 @@
 "use client";
-
 import { useRouter } from "next/navigation";
-import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { twMerge } from "tailwind-merge";
+import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
 
 import useAuthModal from "@/hooks/useAuthMode";
+import { useUser } from "@/hooks/useUser";
 
 import Button from "./Button";
+import { FaUserAlt } from "react-icons/fa";
 
 interface HeaderProps {
   children: React.ReactNode;
@@ -19,8 +21,17 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
   const authModal = useAuthModal();
   const router = useRouter();
 
-  const handleLogout = () => {
-    // Handle logout in the future
+  const supabaseClient = useSupabaseClient();
+  const { user } = useUser();
+
+  const handleLogout = async () => {
+    const { error } = await supabaseClient.auth.signOut();
+    //reset any playing song
+    router.refresh();
+
+    if (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -119,40 +130,52 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
           gap-x-4
         "
         >
-          <>
-            <div>
-              <Button
-                onClick={authModal.onOpen}
-                className="
+          {user ? (
+            <div className="flex gap-x-4 items-center">
+              <Button onClick={handleLogout} className="bg-white px-6 py-2">
+                Logout
+              </Button>
+              <Button>
+                <FaUserAlt />
+              </Button>
+
+
+            </div>
+          ) : (
+            <>
+              <div>
+                <Button
+                  onClick={authModal.onOpen}
+                  className="
                 
                 text-black
                 bg-emerald-600
                 px-6
                 py-2
               "
-              >
-                Sign Up
-              </Button>
-            </div>
-            <div>
-              <Button
-                onClick={authModal.onOpen}
-                className="
+                >
+                  Sign Up
+                </Button>
+              </div>
+              <div>
+                <Button
+                  onClick={authModal.onOpen}
+                  className="
                 bg-emerald-600            
                 text-black
                 px-6
                 py-2
               "
-              >
-                Log in
-              </Button>
-            </div>
-          </>
+                >
+                  Log in
+                </Button>
+              </div>
+            </>
+          )}
         </div>
       </div>
       {children}
     </div>
   );
 };
-
 export default Header;
