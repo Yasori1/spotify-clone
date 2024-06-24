cb0c7cebb550faeb651b423e0555ab41f1859335
Eren <yasar451278@gmail.com>
2024-06-25 01:48:35 +0300

    up

--- components/UploadModal.tsx
+++ components/UploadModal.tsx
@@ -1,16 +1,51 @@
-import Modal from "./Modal";
+"use client";
 
-const UploadModal= () =>{
-    return(
-        <Modal
-        title="Upload modal title"
-        description="Upload modal description"
-        isOpen
-        onChange={() => {}}
-        >
-            Upload Content
-        </Modal>
-    )
-}
+import { FieldValues, useForm } from "react-hook-form";
+
+import useUploadModal from "@/hooks/useUploadModal";
+
+import Modal from "./Modal";
+import { title } from "process";
+
+const UploadModal = () => {
+  const uploadModal = useUploadModal();
+
+  const {
+    register,
+    handleSubmit,
+    reset   
+  } = useForm<FieldValues>({
+    defaultValues: {
+        author:'',
+        title: '',
+        song : null,
+        image : null,
+    }
+  })
+
+  const onChange = (open: boolean) => {
+    if (!open) {
+        reset();
+      uploadModal.onClose();
+    }
+  }
+
+  const onSubmit = () => {
+    // Upload to supabase
+  }
+
+
+
+  return (
+    <Modal
+      title="Add a song"
+      description="Upload an mp3 file"
+      isOpen={uploadModal.isOpen}
+      onChange={onChange}
+    >
+      Form
+    </Modal>
+  );
+};
 
 export default UploadModal;
