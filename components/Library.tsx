06e5ff2d310649dc5fc3c1b15b5e3da120632e3d
Eren <yasar451278@gmail.com>
2024-06-24 20:08:08 +0300

    up

--- components/Library.tsx
+++ components/Library.tsx
@@ -3,9 +3,24 @@
 import { TbPlaylist } from "react-icons/tb";
 import { AiOutlinePlus } from "react-icons/ai";
 
+import useAuthModal from "@/hooks/useAuthMode";
+import { useUser } from "@/hooks/useUser";
+import useUploadModal from "@/hooks/useUploadModal";
+
 const Library = () => {
+  const authModal= useAuthModal();
+  const { user } =useUser();
+  const uploadModal = useUploadModal();
+
   const onClick = () => {
-    // Handle upload later
+    if (!user){
+      return authModal.onOpen(); 
+    }
+
+    // TODO: Check for subsricption
+
+    return uploadModal.onOpen();
+   
   };
 
   return (
