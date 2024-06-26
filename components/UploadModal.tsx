44304e82dbe1747d0916068fff269c157e4a7289
Eren <yasar451278@gmail.com>
2024-06-27 01:47:49 +0300

    up

--- components/UploadModal.tsx
+++ components/UploadModal.tsx
@@ -1,16 +1,23 @@
 "use client";
 
+import uniqid from "uniqid";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import { useState } from "react";
+import toast from "react-hot-toast";
 
 import useUploadModal from "@/hooks/useUploadModal";
+import { useUser } from "@/hooks/useUser";
 
 import Modal from "./Modal";
 import Input from "./Input";
+import Button from "./Button";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
 
 const UploadModal = () => {
   const [isLoading, setIsLoading] = useState(false);
   const uploadModal = useUploadModal();
+  const { user } = useUser();
+  const supabaseClient = useSupabaseClient();
 
   const { register, handleSubmit, reset } = useForm<FieldValues>({
     defaultValues: {
@@ -29,7 +36,45 @@ const UploadModal = () => {
   };
 
   const onSubmit: SubmitHandler<FieldValues> = async (values) => {
-    // Upload to supabase
+    try {
+      setIsLoading(true);
+
+      const imageFile = values.image?.[0];
+      const songFile = values.song?.[0];
+
+      if (!imageFile || !songFile || !user) {
+        toast.error("Missing fields");
+        return;
+      }
+
+      const uniqueID = uniqid();
+
+      // Upload song
+      const { data: songData, error: songError } = await supabaseClient.storage
+        .from("songs")
+        .upload(`song-$(values.title)-${uniqueID}`, songFile, {
+          cacheControl: "3600",
+          upsert: false,
+        });
+
+      if (songError) {
+        setIsLoading(false);
+        return toast.error("Failed song upload");
+      }
+
+      // Upload image
+      const { data: imageData, error: imageError } =
+        await supabaseClient.storage
+          .from("songs")
+          .upload(`song-$(values.title)-${uniqueID}`, songFile, {
+            cacheControl: "3600",
+            upsert: false,
+          });
+    } catch (error) {
+      toast.error("Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
@@ -39,7 +84,10 @@ const UploadModal = () => {
       isOpen={uploadModal.isOpen}
       onChange={onChange}
     >
-      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-y-3">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col gap-y-3 text-red-900"
+      >
         <Input
           id="title"
           disabled={isLoading}
@@ -53,16 +101,28 @@ const UploadModal = () => {
           placeholder="Song author"
         />
         <div>
-          <div className="pb-1">Select a song file</div>
+          <div className="pb-1 text-orange-500">Select a song file</div>
           <Input
             id="Song"
-            type = "file"
+            type="file"
             disabled={isLoading}
             accept=".mp3"
-            {...(register("Song"), { required: true })}            
+            {...(register("Song"), { required: true })}
           />
-          
         </div>
+        <div>
+          <div className="pb-1 text-orange-500">Select an image</div>
+          <Input
+            id="image"
+            type="file"
+            disabled={isLoading}
+            accept="image/*"
+            {...(register("image"), { required: true })}
+          />
+        </div>
+        <Button disabled={isLoading} type="submit">
+          Create
+        </Button>
       </form>
     </Modal>
   );
